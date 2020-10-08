f6a9dea753509be31c7fcf3b8088f4168408f335
Green524 <c17680388727@163.com>
2020-10-08 19:34:47 +0800

    第六次提交(尝试合并)

--- git-demo-1/src/com/chen/TestMain1.java
+++ git-demo-1/src/com/chen/TestMain1.java
@@ -3,5 +3,15 @@ package com.chen;
 public class TestMain1 {
     public static void main(String[] args) {
         System.out.println("第一次提交");
+        char[] arr1 = new char[]{'c',' ',' ','p','q'};
+        char[] arr2 = new char[10];
+        for(char i = 0;i<arr1.length;i++){
+            if(arr1[i] != 32){
+                arr2[i] = arr1[i];
+            }
+        }
+        for (char c : arr2) {
+            System.out.println(c);
+        }
     }
 }
